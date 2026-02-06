045f0712bb6f20b5be032500f8623482fa465f5d
noman1311 <siyam1311@gmail.com>
2026-02-07 00:49:20 +0600

    landing page

---
+++ frontend/src/dashboards/EmergencyDashboard.jsx
@@ -0,0 +1,10 @@
+const EmergencyDashboard = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Emergency Handler Dashboard</h1>
+      <p>Welcome to the emergency handler dashboard. This is under development.</p>
+    </div>
+  );
+};
+
+export default EmergencyDashboard;
